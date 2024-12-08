72b8312e0f20fffff8698822c4d8aa3364dd34b8
AmrTamer23 <amrtamer23mj@gmail.com>
2024-12-08 21:51:36 +0200

    feat: highlight exact char matches with green color

--- src/file_utils.rs
+++ src/file_utils.rs
@@ -6,16 +6,21 @@ pub fn check_if_exists(file_path: &str) -> bool {
 
     is_existing
 }
-
 pub fn search_in_file(word: &str, file: &str) -> Vec<String> {
     let mut file = File::open(file).unwrap();
     let mut content = String::new();
     file.read_to_string(&mut content).unwrap();
+
     let matches: Vec<String> = content
-        .split('\n')
+        .lines()
         .filter(|line| line.contains(word))
-        .map(String::from)
+        .map(|line| highlight_match(line, word))
         .collect();
 
     matches
+}
+
+fn highlight_match(line: &str, word: &str) -> String {
+    let highlighted_word = format!("\x1b[32m{}\x1b[0m", word);
+    line.replace(word, &highlighted_word)
 }
